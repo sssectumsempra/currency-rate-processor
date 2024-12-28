6f35a9b75e401ee83c97a6e0d7560b45966928c7
Александр <alexandrsinko2000@gmail.com>
2024-12-28 11:09:22 +0200

    Initial commit

---
+++ src/main/java/ex/rate/app/ExchangeRateAppApplication.java
@@ -0,0 +1,13 @@
+package ex.rate.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateAppApplication.class, args);
+	}
+
+}
