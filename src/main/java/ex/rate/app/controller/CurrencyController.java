30350e0590c3ff477d3abf473c563907690f4dca
Александр <alexandrsinko2000@gmail.com>
2025-01-03 00:28:58 +0200

    add service layer and realize

--- src/main/java/ex/rate/app/controller/CurrencyController.java
+++ src/main/java/ex/rate/app/controller/CurrencyController.java
@@ -4,6 +4,7 @@ import ex.rate.app.dto.CurrencyDto;
 import ex.rate.app.entity.Currency;
 import ex.rate.app.mapper.currency.CurrencyMapper;
 import ex.rate.app.repository.CurrencyJpaRepository;
+import ex.rate.app.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CurrencyController {
 
-    private final CurrencyJpaRepository jpaRepository;
-
-    private final CurrencyMapper mapper;
+    private final CurrencyService service;
 
     @GetMapping(value = "work")
     public String getWelcome(){
@@ -28,12 +27,11 @@ public class CurrencyController {
 
     @GetMapping("{code}")
     public CurrencyDto gutCurrency(@PathVariable("code") String currencyCode) {
-        Currency currency = jpaRepository.findByCurrencyCode(currencyCode);
-        return mapper.map(currency);
+        return service.findByCurrencyCode(currencyCode);
     }
 
     @GetMapping("list")
     public List<CurrencyDto> getCurrencies() {
-        return jpaRepository.findAll().stream().map(mapper::map).toList();
+        return service.findAll();
     }
 }
